0e627c26ad131030b8e9a9a881e636cbb2374bdd
Kaif Alam <kaifalam9580@gmail.com>
2025-07-05 17:10:42 +0530

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,25 @@ import {
   SignUp,
   SignedIn,
   SignedOut,
-  UserButton
+  UserButton,
+  useUser
 } from "@clerk/clerk-react";
 
 function App() {
+  const user = useUser()
+
+  console.log(user)
+
   return (
     <div style={{ padding: 50 }}>
-      <SignedOut>
-        <h2>Sign In</h2>
-        <SignIn />
-
-        <h2>Sign Up</h2>
-        <SignUp />
-      </SignedOut>
-
-      <SignedIn>
-        <h2>Welcome! 🎉</h2>
-        <UserButton afterSignOutUrl="/" />
-      </SignedIn>
+      <div>
+        <SignedOut>
+          <SignIn />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </div>
   );
 }
